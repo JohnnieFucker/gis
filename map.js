0f60251ba88459d4d817ddbdde99492eb5adadd3
JohnnieFucker <shine@vip.qq.com>
2026-01-10 21:22:01 +0800

    1

--- map.js
+++ map.js
@@ -15,19 +15,57 @@ let selectedVehicleId = null;
 // 不再需要轨迹类型选择
 let refreshInterval = null; // 定时刷新间隔ID
 
-// 创建自定义的ArcGIS World Imagery瓦片图层类
-const ArcGISTileLayer = L.TileLayer.extend({
+// WGS84 转 GCJ-02 (火星坐标系) 算法
+const CoordTransform = {
+    PI: 3.1415926535897932384626,
+    a: 6378245.0,
+    ee: 0.00669342162296594323,
+
+    transformlat: function (lng, lat) {
+        let ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + 0.1 * lng * lat + 0.2 * Math.sqrt(Math.abs(lng));
+        ret += ((20.0 * Math.sin(6.0 * lng * this.PI) + 20.0 * Math.sin(2.0 * lng * this.PI)) * 2.0) / 3.0;
+        ret += ((20.0 * Math.sin(lat * this.PI) + 40.0 * Math.sin((lat / 3.0) * this.PI)) * 2.0) / 3.0;
+        ret += ((160.0 * Math.sin((lat / 12.0) * this.PI) + 320 * Math.sin((lat * this.PI) / 30.0)) * 2.0) / 3.0;
+        return ret;
+    },
+
+    transformlng: function (lng, lat) {
+        let ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + 0.1 * lng * lat + 0.1 * Math.sqrt(Math.abs(lng));
+        ret += ((20.0 * Math.sin(6.0 * lng * this.PI) + 20.0 * Math.sin(2.0 * lng * this.PI)) * 2.0) / 3.0;
+        ret += ((20.0 * Math.sin(lng * this.PI) + 40.0 * Math.sin((lng / 3.0) * this.PI)) * 2.0) / 3.0;
+        ret += ((150.0 * Math.sin((lng / 12.0) * this.PI) + 300.0 * Math.sin((lng / 30.0) * this.PI)) * 2.0) / 3.0;
+        return ret;
+    },
+
+    wgs84togcj02: function (lng, lat) {
+        lat = parseFloat(lat);
+        lng = parseFloat(lng);
+        let dlat = this.transformlat(lng - 105.0, lat - 35.0);
+        let dlng = this.transformlng(lng - 105.0, lat - 35.0);
+        let radlat = (lat / 180.0) * this.PI;
+        let magic = Math.sin(radlat);
+        magic = 1 - this.ee * magic * magic;
+        let sqrtmagic = Math.sqrt(magic);
+        dlat = (dlat * 180.0) / (((this.a * (1 - this.ee)) / (magic * sqrtmagic)) * this.PI);
+        dlng = (dlng * 180.0) / ((this.a / sqrtmagic) * Math.cos(radlat) * this.PI);
+        let mglat = lat + dlat;
+        let mglng = lng + dlng;
+        return [mglng, mglat];
+    }
+};
+
+// 创建自定义的高德瓦片图层类
+const GaodeTileLayer = L.TileLayer.extend({
     getTileUrl: function (coords) {
-        const x = Math.floor(coords.x);
-        const y = Math.floor(coords.y);
+        const x = coords.x;
+        const y = coords.y;
         const z = coords.z;
-        const url = `https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/${z}/${y}/${x}`;
-        return url;
+        return `https://webst01.is.autonavi.com/appmaptile?style=6&x=${x}&y=${y}&z=${z}`;
     }
 });
 
-function arcgisTileLayer(options) {
-    return new ArcGISTileLayer("", options);
+function gaodeTileLayer(options) {
+    return new GaodeTileLayer("", options);
 }
 
 // 初始化地图
@@ -39,9 +77,9 @@ function initMap() {
         maxZoom: MAP_CONFIG.maxZoom
     });
 
-    // 添加ArcGIS地图图层
-    arcgisTileLayer({
-        attribution: '&copy; <a href="https://www.esri.com/">Esri</a>',
+    // 添加高德地图图层
+    gaodeTileLayer({
+        attribution: '&copy; <a href="https://www.amap.com/">高德地图</a>',
         maxZoom: MAP_CONFIG.maxZoom,
         minZoom: MAP_CONFIG.minZoom,
         tileSize: 256
@@ -94,8 +132,8 @@ async function fetchAllVehicles() {
                 id: result.device_id,
                 name: result.device_name,
                 currentLocation: {
-                    lat: result.realtime_datas.latitude,
-                    lng: result.realtime_datas.longitude
+                    lat: CoordTransform.wgs84togcj02(result.realtime_datas.longitude, result.realtime_datas.latitude)[1],
+                    lng: CoordTransform.wgs84togcj02(result.realtime_datas.longitude, result.realtime_datas.latitude)[0]
                 },
                 lastUpdateTime: updateTime
             };
